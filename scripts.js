3a6ac1559ade11cc9ca25bf9d883fb4363009dee
Tyler Noblett <tylernoblett@Tylers-MacBook-Pro.local>
2020-03-17 10:51:49 -0400

    finish refactoring

--- scripts.js
+++ scripts.js
@@ -11,7 +11,7 @@ const cameraOutput = document.querySelector("#camera--output")
 const cameraSensor = document.querySelector("#camera--sensor")
 const cameraTrigger = document.querySelector("#camera--trigger")
 
-const cameraStart = () => {
+const cameraStart = async () => {
     navigator.mediaDevices
         .getUserMedia(constraints)
         .then((stream) => {
@@ -23,59 +23,65 @@ const cameraStart = () => {
         });
 }
 
-const dataURItoBlob = (dataURI) => {
-    // convert base64/URLEncoded data component to raw binary data held in a string
-    var byteString;
-    if (dataURI.split(',')[0].indexOf('base64') >= 0)
-        byteString = atob(dataURI.split(',')[1]);
-    else
-        byteString = unescape(dataURI.split(',')[1]);
-
-    // separate out the mime component
-    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
-
-    // write the bytes of the string to a typed array
-    var ia = new Uint8Array(byteString.length);
-    for (var i = 0; i < byteString.length; i++) {
-        ia[i] = byteString.charCodeAt(i);
-    }
-
-    return new Blob([ia], {
-        type: mimeString
+const URIToBlob = (dataURI) => {
+    const mimeType = dataURI.split(',')[0].split(':')[1].split(';')[0];
+    const binary = atob(dataURI.split(',')[1]);
+    const array = [];
+    for (var i = 0; i < binary.length; i++) array.push(binary.charCodeAt(i));
+    return new Blob([new Uint8Array(array)], {
+        type: mimeType
     });
 }
 
-setType = (obj) => {
+const setType = (obj) => {
     document.getElementById("content").innerHTML = obj.content;
     document.getElementById("type").innerHTML = obj.type;
 }
 
+const setSensors = () => {
+    cameraSensor.width = cameraView.videoWidth;
+    cameraSensor.height = cameraView.videoHeight;
+    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+}
+
+const createFormData = () => {
+    const data = cameraSensor.toDataURL('image/jpeg', 0.5);
+    const formData = new FormData(document.forms[0]);
+    formData.append("file", URIToBlob(data));
+    return formData;
+}
+
+const fetchResponse = async (formData) => {
+    let response = await fetch(url, {
+        method: 'POST',
+        body: formData
+    });
+    response = await response.json();
+    return response;
+}
+
 cameraTrigger.onclick = async () => {
     let loadingText = 'Loading';
-    setType({ content: '', type: loadingText })
+    setType({
+        content: '',
+        type: loadingText
+    });
 
     const loading = setInterval(() => {
         if (loadingText !== 'Loading...') {
-            loadingText = loadingText + '.'
+            loadingText = loadingText + '.';
             document.getElementById("type").innerHTML = loadingText;
         } else {
-            loadingText = 'Loading'
+            loadingText = 'Loading';
             document.getElementById("type").innerHTML = loadingText;
         }
     }, 350);
 
-    cameraSensor.width = cameraView.videoWidth;
-    cameraSensor.height = cameraView.videoHeight;
-    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
-
-    const data = cameraSensor.toDataURL('image/jpeg', 0.5);
-    const formData = new FormData(document.forms[0]);
-    formData.append("file", dataURItoBlob(data));
-
-    let response = await fetch(url, { method: 'POST', body: formData });
-    response = await response.json();
+    setSensors();
+    const formData = createFormData();
+    const response = await fetchResponse(formData);
     clearInterval(loading);
-    setType(response)
+    setType(response);
 };
 
 window.addEventListener("load", cameraStart, false);

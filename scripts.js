d2b3048af21c8bbadfff840587eb6cc2dd41f50e
tylernoblett <tyler.noblett@outlook.com>
2020-04-20 10:25:20 -0400

    make var name more clear

--- scripts.js
+++ scripts.js
@@ -25,9 +25,9 @@ const cameraStart = async () => {
 const URIToBlob = (dataURL) => {
     const mimeType = dataURL.split(',')[0].split(':')[1].split(';')[0];
     const binary = atob(dataURL.split(',')[1]);
-    const array = [];
-    for (var i = 0; i < binary.length; i++) array.push(binary.charCodeAt(i));
-    return new Blob([new Uint8Array(array)], {
+    const arrOfBytes = [];
+    for (var i = 0; i < binary.length; i++) arrOfBytes.push(binary.charCodeAt(i));
+    return new Blob([new Uint8Array(arrOfBytes)], {
         type: mimeType
     });
 }
